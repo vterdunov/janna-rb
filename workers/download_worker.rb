fd5091460bc51152e21d15459662f73e94c1523d
vterdunov <mail2slick@gmail.com>
2016-10-08 20:39:44 +0300

    Files will download in unique directories.

--- workers/download_worker.rb
+++ workers/download_worker.rb
@@ -5,24 +5,27 @@ class DownloadWorker
 
   def perform(url)
     @url = url
-    file_name = download_ova_job @url
-    store filename: file_name
+    ova_path = download_ova_job @url
+    store ova_path: ova_path
   end
 
   def download_ova_job(url)
     uri = URI(url)
     file_name = filename
+    tmp_file = "#{tmp_dir}/#{file_name}"
     Net::HTTP.start(uri.host, uri.port) do |http|
       request = Net::HTTP::Get.new uri
       http.request request do |response|
-        open "/data/#{file_name}", 'w' do |io|
+        open tmp_file, 'w' do |io|
           response.read_body do |chunk|
             io.write chunk
+            puts 'waiting...'
           end
         end
       end
     end
-    file_name.to_s
+    puts "File Downloaded: #{tmp_file}"
+    tmp_file
   rescue
     'Unexpected error'
   end
@@ -33,6 +36,10 @@ class DownloadWorker
   end
 
   private
+
+  def tmp_dir
+    Dir.mktmpdir('janna-tmp-download-', '/tmp')
+  end
 
   def filename
     uri = URI(@url)
--- workers/prepare_worker.rb
+++ workers/prepare_worker.rb
@@ -9,23 +9,26 @@ class PrepareWorker
   end
 
   def wait_for_job(jid)
+    puts Sidekiq::Status.complete?(jid)
     while Sidekiq::Status.complete?(jid) == false
       puts "waiting for #{jid} will be complete."
       sleep 1
     end
+    puts Sidekiq::Status.complete?(jid)
     puts "#{jid} complete"
     unpack_ova jid
   end
 
   def unpack_ova(jid)
     j_data = Sidekiq::Status.get_all jid
-    ova_path = "/data/#{j_data['filename']}"
-    puts ova_path
+    ova_path = j_data['ova_path']
+    ova_dir = File.dirname(ova_path)
+    puts "OVA_PATH: #{ova_path} OVA_DIR: #{ova_dir}"
     begin
       dir = Dir.mktmpdir('janna-', '/tmp')
       `tar xf #{ova_path} -C #{dir}`
-      if File.readable?(ova_path) && File.exist?(ova_path)
-        File.delete ova_path
+      if File.readable?(ova_dir) && File.exist?(ova_dir)
+        # FileUtils.rm_rf ova_dir
         200
       else
         [500, 'Unpack error']
