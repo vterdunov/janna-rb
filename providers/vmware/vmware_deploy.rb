2dafc702740e2fd2fe7295696c68354e529b3aed
VTerdunov <mail2slick@gmail.com>
2016-10-19 21:09:01 +0300

    Decomposed deploy class

--- providers/vmware/vmware_deploy.rb
+++ providers/vmware/vmware_deploy.rb
@@ -11,83 +11,89 @@ class VMwareDeploy
     @ovf_path = ovf_path
     @vm_name = vm_name
     @opts = opts
+
+    @template_folder_path = opts[:template_path]
+    @template_name = @opts[:template_name]
   end
 
   def start
-    # puts @ovf_path
-    # puts @vm_name
-    # puts @opts
-
+    # config
     vim = VIM.connect @opts
     dc = vim.serviceInstance.find_datacenter(@opts[:datacenter])
-    template_folder_path = @opts[:template_path]
-    template_name = @opts[:template_name]
     root_vm_folder = dc.vmFolder
-    vm_folder = root_vm_folder
 
-    if @opts[:vm_folder_path]
-      vm_folder = root_vm_folder.traverse(@opts[:vm_folder_path], VIM::Folder)
-    end
-    template_folder = root_vm_folder.traverse!(template_folder_path, VIM::Folder)
+    vm_folder = root_vm_folder.traverse(@opts[:vm_folder_path], VIM::Folder)
+    template_folder = root_vm_folder.traverse!(@template_folder_path, VIM::Folder)
 
     scheduler = AdmissionControlledResourceScheduler.new(
       vim,
-      :datacenter => dc,
-      :computer_names => [@opts[:computer_path]],
-      :vm_folder => vm_folder,
-      :rp_path => '/',
-      :datastore_paths => [@opts[:datastore]],
-      :max_vms_per_pod => nil, # No limits
-      :min_ds_free => nil, # No limits
+      datacenter: dc,
+      computer_names: [@opts[:computer_path]],
+      vm_folder: vm_folder,
+      rp_path: '/',
+      datastore_paths: [@opts[:datastore]],
+      max_vms_per_pod: nil, # No limits
+      min_ds_free: nil, # No limits
     )
     scheduler.make_placement_decision
 
     datastore = scheduler.datastore
     computer = scheduler.pick_computer
-    # XXX: Do this properly
-    if @opts[:network]
-      network = computer.network.find{|x| x.name == @opts[:network]}
-    else
-      network = computer.network[0]
-    end
+    network = computer.network.find { |x| x.name == @opts[:network] }
 
-    lease_tool = LeaseTool.new
-    lease = @opts[:lease] * 24 * 60 * 60
-    deployer = CachedOvfDeployer.new(
+    @lease_tool = LeaseTool.new
+    @lease = @opts[:lease] * 24 * 60 * 60
+
+    @deployer = CachedOvfDeployer.new(
       vim, network, computer, template_folder, vm_folder, datastore
     )
-    template = deployer.lookup_template template_name
+    template = @deployer.lookup_template @template_name
 
-    if !template
-      puts "#{Time.now}: Uploading/Preparing OVF template ..."
+    # ----------------------------------------
+    template = deploy_template template
 
-      template = deployer.upload_ovf_as_template(
-        @ovf_path, template_name,
-        :run_without_interruptions => true,
-        :config => lease_tool.set_lease_in_vm_config({}, lease)
+    vm = clone_vm_from_template template
+
+    powerup_vm vm
+  end
+
+  private
+
+  def deploy_template(template)
+    unless template
+      puts 'Uploading/Preparing OVF template ...'
+
+      template = @deployer.upload_ovf_as_template(
+        @ovf_path, @template_name,
+        run_without_interruptions: true,
+        config: @lease_tool.set_lease_in_vm_config({}, @lease)
       )
     end
+    template
+  end
 
-    puts "#{Time.now}: Cloning template ..."
+  def clone_vm_from_template(template)
+    puts 'Cloning template ...'
     config = {
-      :numCPUs => @opts[:cpus],
-      :memoryMB => @opts[:memory],
+      numCPUs: @opts[:cpus],
+      memoryMB: @opts[:memory]
     }
-    config = lease_tool.set_lease_in_vm_config(config, lease)
-    vm = deployer.linked_clone template, @vm_name, config
+    config = @lease_tool.set_lease_in_vm_config(config, @lease)
+    @deployer.linked_clone template, @vm_name, config
+  end
 
-    puts "#{Time.now}: Powering On VM ..."
-    # XXX: Add a retrying version?
+  def powerup_vm(vm)
+    puts 'Powering On VM ...'
     vm.PowerOnVM_Task.wait_for_completion
 
-    puts "#{Time.now}: Waiting for VM to be up ..."
-    ip = nil
-    while !(ip = vm.guest_ip)
+    puts 'Waiting for VM to be up ...'
+    until (ip = vm.guest_ip)
       sleep 5
     end
 
-    puts "#{Time.now}: VM got IP: #{ip}"
+    puts "VM got IP: #{ip}"
+    puts 'Done'
 
-    puts "#{Time.now}: Done"
+    ip
   end
 end
