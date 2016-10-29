f2df7fcd13b383219804ad548b62c0aefde3f06b
vterdunov <mail2slick@gmail.com>
2016-10-29 13:05:28 +0300

    Continue work to move modular style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,27 @@
 require 'sinatra/base'
+require 'redis'
+require 'sidekiq'
 
 class ApplicationController < Sinatra::Base
   helpers ApplicationHelper
 
-  # configure do
-    # set :bind, '0.0.0.0'
-    # enable :logging
+  configure do
+    set :bind, '0.0.0.0'
+    enable :logging
+    REDIS = Redis.new(url: ENV['REDIS_URI'])
 
-    # REDIS = Redis.new(url: ENV['REDIS_URI'])
+    set vsphere_address:        ENV['VSPHERE_ADDRESS']
+    set vsphere_username:       ENV['VSPHERE_USERNAME']
+    set vsphere_password:       ENV['VSPHERE_PASSWORD']
+    set vsphere_dc:             ENV['VSPHERE_DC']
+    set vsphere_datastore:      ENV['VSPHERE_DATASTORE']
+    set vsphere_network:        ENV['VSPHERE_NETWORK']
+    set vsphere_template_path:  ENV['VSPHERE_TEMPLATE_PATH']
+    set vsphere_computer_path:  ENV['VSPHERE_COMPUTER_PATH']
+    set vsphere_vm_folder_path: ENV['VSPHERE_VM_FOLDER_PATH']
 
-    # Sidekiq.configure_server do |config|
-    #   config.redis = { url: ENV['REDIS_URI'] }
-    # end
-
-    # Sidekiq.configure_client do |config|
-    #   config.redis = { url: ENV['REDIS_URI'] }
-    # end
-  # end
+    set slack_webhook_url:  ENV['SLACK_WEBHOOK_URL']
+    set slack_channel:      ENV['SLACK_CHANNEL']
+    set slack_bot_username: ENV['SLACK_BOT_USERNAME']
+  end
 end
-
--- config.ru
+++ config.ru
@@ -1,10 +1,26 @@
+require 'dotenv'
+Dotenv.load(
+  File.expand_path('../.env.local', __FILE__),
+  File.expand_path("../.env.#{ENV['RACK_ENV']}", __FILE__),
+  File.expand_path('../.env', __FILE__))
+
 require 'sinatra/base'
 require 'bundler/setup'
 
 require './app/helpers/application_helper.rb'
 require './app/controllers/application_controller.rb'
 
-Dir.glob("#{__dir__}/app/{controllers,helpers}/*.rb").each { |file| require file }
+Dir.glob("#{__dir__}/app/{controllers,helpers,lib}/*.rb").each { |file| require file }
+Dir.glob("#{__dir__}/lib/**/*.rb").each { |file| require file }
+
+require 'sidekiq'
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URI'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URI'] }
+end
 
 map('/')       { run ApplicationController }
 map('/health') { run HealthController }
