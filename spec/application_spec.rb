63cad7d88e90b20831702bbad6e53f0df0b87f9b
vterdunov <mail2slick@gmail.com>
2016-12-01 23:27:05 +0300

    Fixed test

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'ApplicationController' do
     it { is_expected.to be_ok }
   end
 
-  context 'POST /vm' do
-    subject { post 'vm', args }
-    let(:args) { { address: 'asdf', vmname: 'asdf' } }
+  context 'POST /v1/vm' do
+    subject { post 'v1/vm', args }
+    let(:args) { { address: 'asdf', vmname: 'asdf', provider_type: 'dummy' } }
     it { expect(subject.status).to eq 202 }
   end
 end
