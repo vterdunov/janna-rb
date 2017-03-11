4ca127385461146924fc41a70341233ea0589928
Bobykin Kirill <qelphybox@gmail.com>
2017-03-11 23:42:22 +0300

    Refactored rest api implementation

---
+++ lib/helpers/rest_helper.rb
@@ -0,0 +1,32 @@
+require_relative '../workers/vmware_deploy_ova'
+require_relative '../workers/vmware_destroy_vm'
+require_relative '../services/providers/vmware/vmware_vm'
+require_relative '../workers/vmware_deploy_template'
+
+Dummy = Struct.new(:perform_async, :new) do
+  def perform_async(*asdf); end
+  def new(*asdf); end
+end
+
+module RestHelper
+  VM_PARAMS = %i(vmname ova_url network datacenter datastore cluster vm_folder message_to)
+
+  PROVIDER_TYPES = {
+    ['vmvare', 'post',   '/v1/vm']       => VMwareDeployOVA,
+    ['vmvare', 'delete', '/v1/vm']       => VMwareDestroyVM,
+    ['vmvare', 'get',    '/v1/vm']       => VMwareVM,
+    ['vmvare', 'post',   '/v1/template'] => VMwareDeployTemplate
+  }
+
+  def provider_worker
+    PROVIDER_TYPES[[params[:provider_type], params[:action], params[:path]]] || Dummy.new
+  end
+
+  # @return [Hash] params for executor-object
+  def vm_params
+    custom = params.slice(*VM_PARAMS).each_with_object({}) do |(key, value), result|
+      result[key.to_sym] = value.strip
+    end
+    $default_vm_params.merge(custom)
+  end
+end
--- lib/services/providers/vmware/vmware_vm.rb
+++ lib/services/providers/vmware/vmware_vm.rb
@@ -9,26 +9,7 @@ class VMwareVM
 
   def initialize(opts)
     @vm_name = opts[:vmname]
-    @opts    = defaults.merge(opts)
-  end
-
-  def defaults
-    {
-      host:           ENV['VSPHERE_ADDRESS'],
-      port:           ENV['VSPHERE_PORT'],
-      user:           ENV['VSPHERE_USERNAME'],
-      password:       ENV['VSPHERE_PASSWORD'],
-      datacenter:     ENV['VSPHERE_DC'],
-      datastore:      ENV['VSPHERE_DATASTORE'],
-      computer_path:  ENV['VSPHERE_CLUSTER'],
-      network:        ENV['VSPHERE_NETWORK'],
-      vm_folder_path: ENV['VSPHERE_VM_FOLDER'],
-      template_path:  ENV['VSPHERE_TEMPLATE_PATH'],
-      path:           '/sdk',
-      insecure:       true,
-      ssl:            true,
-      debug:          false
-    }
+    @opts    = opts
   end
 
   # return [Array] Array of VM IP addresss
