9dc5fbc21e0e59dd8b55df71e8e326c2328ab246
vterdunov <mail2slick@gmail.com>
2017-06-25 00:40:54 +0300

    Increased default job time expiration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@ Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URI'] }
   config.server_middleware do |chain|
     # accepts :expiration (optional)
-    chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
+    chain.add Sidekiq::Status::ServerMiddleware, expiration: 180.minutes # default
   end
   config.client_middleware do |chain|
     # accepts :expiration (optional)
-    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
+    chain.add Sidekiq::Status::ClientMiddleware, expiration: 180.minutes # default
   end
 end
 
@@ -14,6 +14,6 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URI'] }
   config.client_middleware do |chain|
     # accepts :expiration (optional)
-    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
+    chain.add Sidekiq::Status::ClientMiddleware, expiration: 180.minutes # default
   end
 end
