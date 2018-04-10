0d7cf37909f613aed84a7fe0e4e6882212b4104d
vterdunov <mail2slick@gmail.com>
2018-04-10 15:33:10 +0300

    Reduce Unpacker responsibility

--- lib/services/unpacker.rb
+++ lib/services/unpacker.rb
@@ -1,26 +1,8 @@
 class Unpacker
-  def initialize(path)
-    @path = path
-  end
-
-  def tar
-    tmp_dir = unpack @path
-    cleanup @path
-    tmp_dir
-  rescue
-    [500, 'Prepare error']
-  end
-
-  def unpack(path)
+  def untar(file)
     t_dir = Dir.mktmpdir('janna-', '/tmp')
-    $logger.debug { "Unpack file, src=#{path}, dest=#{t_dir}" }
-    `tar xf #{path} -C #{t_dir}`
+    $logger.debug { "Unpack file, src=#{file}, dest=#{t_dir}" }
+    `tar xf #{file} -C #{t_dir}`
     t_dir
-  end
-
-  def cleanup(path)
-    dir_name = File.dirname(path)
-    raise 'Cannot cleanup' unless File.readable?(dir_name) && File.exist?(dir_name)
-    FileUtils.rm_rf dir_name
   end
 end
